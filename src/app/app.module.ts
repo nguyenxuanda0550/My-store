7458f051ca469f07524565f520f2cf91ea0ee67d
Da <nxd113322@gmail.com>
2023-07-12 22:38:15 +0700

    Set up env

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,22 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
+import { BrowserModule } from '@angular/platform-browser';
+// import { CartComponent } from './cart/cart.component';
+import { ConfirmationComponent } from './components/confirmation/confirmation.component';
+import { HeaderComponent } from './layout/header/header.component';
+import { NgModule } from '@angular/core';
+import { ProductItemComponent } from './components/product-item/product-item.component';
+import { ProductItemDetailComponent } from './components/product-item-detail/product-item-detail.component';
+import { ProductListComponent } from './components/product-list/product-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ConfirmationComponent,
+    ProductItemComponent,
+    ProductItemDetailComponent,
+    ProductListComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
